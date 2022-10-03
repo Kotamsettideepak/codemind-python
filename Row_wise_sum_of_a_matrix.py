56b87c12e7b9f2d8c0881ac3712bce265525ddd1
Kotamsettideepak <98525642+Kotamsettideepak@users.noreply.github.com>
2022-10-03 11:18:38 +0530

    a,b=map(int,input().split()) l=[] for i in range(a):     le=list(input().split())     le=[eval(i) for i in le]     l.append(le) sm=0 for i in range(a):     for j in range(b):         sm=sm+l[i][j]     print(sm,end=' ')     sm=0

---
+++ Row_wise_sum_of_a_matrix.py
@@ -0,0 +1,12 @@
+a,b=map(int,input().split())
+l=[]
+for i in range(a):
+    le=list(input().split())
+    le=[eval(i) for i in le]
+    l.append(le)
+sm=0
+for i in range(a):
+    for j in range(b):
+        sm=sm+l[i][j]
+    print(sm,end=' ')
+    sm=0
