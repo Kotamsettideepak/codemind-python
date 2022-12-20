f5e19ae3695e83aa4e885e804492ae62cb28075e
Kotamsettideepak <98525642+Kotamsettideepak@users.noreply.github.com>
2022-12-21 01:00:13 +0530

    a=int(input()) for i in range(1,a+1):     for j in range(1,i+1):         print(j,end='')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,37 +1,5 @@
 a=int(input())
-l=[[0]*a]*a
-if a%2 != 0:
-    for i in range(a//2):
-        b=list(l[i])
-        b[-i-1]='x'
-        b[i]='x'
-        c=list(b)
-        l[i]=c
-        b.clear()
-    l[a//2][a//2]='x'
-    for i in range(a//2):
-        l[-i-1]=l[i]
-    for i in range(len(l)):
-        b=list(l[i])
-        for j in range(len(b)):
-            print(b[j],end='')
-        print()
-else :
-    for i in range((a//2)-1):
-        b=list(l[i])
-        b[-i-1]='x'
-        b[i]='x'
-        c=list(b)
-        l[i]=c
-        b.clear()   
-    l[a//2][a//2]='x'
-    l[a//2][(a//2)-1]='x'
-    l[(a//2)-1][a//2]='x'
-    l[(a//2)-1][(a//2)-1]='x'
-    for i in range(a//2):
-        l[-i-1]=l[i]
-    for i in l:
-        a=list(i)
-        for j in range(len(a)):
-            print(a[j],end='')
-        print()
+for i in range(1,a+1):
+    for j in range(1,i+1):
+        print(j,end='')
+    print()
