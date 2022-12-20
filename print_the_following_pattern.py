1b3a250b867bd7987a52d0642fa1067236b35616
Kotamsettideepak <98525642+Kotamsettideepak@users.noreply.github.com>
2022-12-21 00:47:09 +0530

    a=int(input()) l=[[0]*a]*a if a%2 != 0:     for i in range(a//2):         b=list(l[i])         b[-i-1]='x'         b[i]='x'         c=list(b)         l[i]=c         b.clear()     l[a//2][a//2]='x'     for i in range(a//2):         l[-i-1]=l[i]     for i in range(len(l)):         b=list(l[i])         for j in range(len(b)):             print(b[j],end='')         print() else :     for i in range((a//2)-1):         b=list(l[i])         b[-i-1]='x'         b[i]='x'         c=list(b)         l[i]=c         b.clear()     l[a//2][a//2]='x'     l[a//2][(a//2)-1]='x'     l[(a//2)-1][a//2]='x'     l[(a//2)-1][(a//2)-1]='x'     for i in range(a//2):         l[-i-1]=l[i]     for i in l:         a=list(i)         for j in range(len(a)):             print(a[j],end='')         print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,13 +1,37 @@
 a=int(input())
-l=[['x']*a]*a
-r=[]
-for i in range(a):
-    b=list(l[i])
-    b[i]=0
-    c=list(b)
-    r.append(c)
-for i in r:
-    a=list(i)
-    for j in range(len(a)):
-        print(a[j],end='')
-    print()
+l=[[0]*a]*a
+if a%2 != 0:
+    for i in range(a//2):
+        b=list(l[i])
+        b[-i-1]='x'
+        b[i]='x'
+        c=list(b)
+        l[i]=c
+        b.clear()
+    l[a//2][a//2]='x'
+    for i in range(a//2):
+        l[-i-1]=l[i]
+    for i in range(len(l)):
+        b=list(l[i])
+        for j in range(len(b)):
+            print(b[j],end='')
+        print()
+else :
+    for i in range((a//2)-1):
+        b=list(l[i])
+        b[-i-1]='x'
+        b[i]='x'
+        c=list(b)
+        l[i]=c
+        b.clear()   
+    l[a//2][a//2]='x'
+    l[a//2][(a//2)-1]='x'
+    l[(a//2)-1][a//2]='x'
+    l[(a//2)-1][(a//2)-1]='x'
+    for i in range(a//2):
+        l[-i-1]=l[i]
+    for i in l:
+        a=list(i)
+        for j in range(len(a)):
+            print(a[j],end='')
+        print()
