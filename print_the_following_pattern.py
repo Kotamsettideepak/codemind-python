45b8d2428d205461a79dc37cee3983a8c35a4fe2
Kotamsettideepak <98525642+Kotamsettideepak@users.noreply.github.com>
2022-12-25 11:49:14 +0530

    a=int(input()) l=[] r=[] for i in range(a):     for j in range(i+1):         r.append(chr(65+i))     b=list(r)     l.append(b)     r.clear() l.reverse() for i in l:     i=list(i)     for j in i:         print(j,end=' ')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,7 +1,15 @@
 a=int(input())
-for j in range(a):
-    b=chr(65+j)
-    c=[b]*a
-    for m in c:
-        print(m,end=' ')
+l=[]
+r=[]
+for i in range(a):
+    for j in range(i+1):
+        r.append(chr(65+i))
+    b=list(r)
+    l.append(b)
+    r.clear()
+l.reverse()
+for i in l:
+    i=list(i)
+    for j in i:
+        print(j,end=' ')
     print()
