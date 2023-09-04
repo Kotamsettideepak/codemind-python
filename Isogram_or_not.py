cc75a652afa9815799664eb9329bdb15ff0473fe
Kotamsettideepak <98525642+Kotamsettideepak@users.noreply.github.com>
2023-09-04 13:57:16 +0530

    def moon(s:str)->bool:     d={}     for i in s:         if i in d:             return False         else:             d[i]=1     return True
    s=input()
    print(moon(s))

--- Isogram_or_not.py
+++ Isogram_or_not.py
@@ -1,11 +1,11 @@
-a=input()
-l=list(a)
-c=0
-for i in range(0,len(l)):
-    for j in range( 0,len(l)):
-        if l[i]==l[j]:
-            c+=1
-if c==len(l):
-    print("True")
-else :
-    print("False")
+def moon(s:str)->bool:
+    d={}
+    for i in s:
+        if i in d:
+            return False
+        else:
+            d[i]=1
+    return True
+
+s=input()
+print(moon(s))
